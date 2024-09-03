ed7b8d554dc8c256b2533546f0f2c42670a594e4
Istionia <timmoyoong678@protonmail.com>
2024-09-03 22:10:27 +0800

    Material UI integration

--- book/1-begin/components/Header.jsx
+++ book/1-begin/components/Header.jsx
@@ -1,10 +1,19 @@
 import Link from 'next/link';
 
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+
+import { styleToolbar } from './SharedStyles';
+
 const Header = () => (
   <div>
-    <Link href="/login" passHref>
-      <a style={{ margin: '0px 20px 0px auto' }}>Log in</a>
-    </Link>
+    <Toolbar style={styleToolbar}>
+      <Grid container direction="row" justifyContent="space-around" align="center">
+        <Grid item xs={12} style={{ textAlign: 'right' }}>
+          <Link href="/login">Log In</Link>
+        </Grid>
+      </Grid>
+    </Toolbar>
   </div>
 );
 
